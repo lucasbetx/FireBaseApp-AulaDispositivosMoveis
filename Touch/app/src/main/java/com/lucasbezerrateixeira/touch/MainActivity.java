16a2c5603ba50b8894009d06be2e863b5425f651
lucasbetx <36702465+lucasbetx@users.noreply.github.com>
2019-06-17 14:45:14 -0300

    trabalho

---
+++ Touch/app/src/main/java/com/lucasbezerrateixeira/touch/MainActivity.java
@@ -0,0 +1,48 @@
+package com.lucasbezerrateixeira.touch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout myLayout = null;
+    private Button luckyID = null;
+
+    private float x;
+    private float y;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        myLayout = (RelativeLayout) findViewById(R.id.myLayout);
+        luckyID = (Button) findViewById(R.id.luckyID);
+
+
+        myLayout.setOnTouchListener(new View.OnTouchListener() {
+            int i = 0;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                x = event.getX();
+
+
+                if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                    luckyID.setX(x);
+                    luckyID.setY(y);
+
+                }
+                if (++i > 50) {
+                    return false;
+                }
+                return true;
+            }
+        });
+    }
+}
